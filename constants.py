c912ef80aa48f8101500d1b0eeeb46459c583b50
Abhay P. Nambiar <nambiar.abhay2000@gmail.com>
2025-06-22 16:45:43 +0100

    added eeg data visualization

--- constants.py
+++ constants.py
@@ -3,3 +3,16 @@ FORGED = "Forged"
 TRAIN = "Train"
 TEST = "Forged"
 VALIDATION = "Val"
+MAT_DATA_HEADERS = ['SubjectID',
+ 'Gen_Forge',
+ 'EEG',
+ 'ICA_EEG',
+ 'Diff',
+ 'SignWacom',
+ 'EEGHeader',
+ 'WacomHeader',
+ 'Note',
+ 'Path1',
+ 'Path2',
+ 'SignerID',
+ 'EpochLength']
