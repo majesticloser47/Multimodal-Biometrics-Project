d254c5f1219f7ecffed880cc9b71fbca8c831a60
Abhay P. Nambiar <nambiar.abhay2000@gmail.com>
2025-06-24 17:07:35 +0100

    features extracted from eeg signal and added code for extracting features for all users

--- constants.py
+++ constants.py
@@ -16,3 +16,9 @@ MAT_DATA_HEADERS = ['SubjectID',
  'Path2',
  'SignerID',
  'EpochLength']
+epoch_frames = 1280 #frames
+epoch_length = 10 #seconds
+number_of_sections = 5
+fps = epoch_frames / epoch_length
+roi_starting_point = number_of_sections * epoch_frames
+roi_idx = [roi_starting_point - 1, -1]
